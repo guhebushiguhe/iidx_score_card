50ce6e18876687a073256de28cc744e16d641ccd
guhe <642046261@qq.com>
2022-03-27 22:32:02 +0800

    更新token，删除法院相关

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -13,7 +13,7 @@ axios.interceptors.request.use(function (config) {
   // console.log(config)
   if(config.url.match('https://arcana.nu')){
     config.headers['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
-    config.headers['authorization'] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJ0b2tlbiIsImlhdCI6MTYxMTU5NTY3MSwic3ViIjoiNjQyMDQ2MjYxQHFxLmNvbSIsInNjb3BlIjoibWFjaGluZV9hY2Nlc3Mgcml2YWxfd3JpdGUgc2V0dGluZ3NfcmVhZCBzZXR0aW5nc193cml0ZSJ9.stRjxVQB_-0nkJnJf4o2ZNGyQGUmOI4RddUKC5BEKZ4'
+    config.headers['authorization'] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJ0b2tlbiIsImlhdCI6MTY0ODM5MTE3Nywic3ViIjoiNjQyMDQ2MjYxQHFxLmNvbSIsInNjb3BlIjoibWFjaGluZV9hY2Nlc3Mgcml2YWxfd3JpdGUgc2V0dGluZ3NfcmVhZCBzZXR0aW5nc193cml0ZSJ9.MoFbqE8NXj0jajPeLbBph1UalGKF_98tlXeEnM-p90k'
   }
   return config;
 }, function (err) {
